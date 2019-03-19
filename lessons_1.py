31af0b50a69870c46ab8abf39dab474a0febd8ad
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-03-19 15:49:12 +0300

    update insert function

--- lessons_1.py
+++ lessons_1.py
@@ -1,102 +1,99 @@
 class Node:
-
-    def __init__(self, v):
-        self.value = v
-        self.next = None
+ def __init__(self, v, next = None):
+    self.value = v
+    self.next = None
 
 class LinkedList:
+ def __init__(self):
+    self.head = None
+    self.tail = None
+    self.leng = 0
 
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.leng = 0
+ def add_in_tail(self, item):
+    if self.head is None:
+     self.head = item
+    else:
+     self.tail.next = item
+    self.tail = item
 
-    def add_in_tail(self, item):
-        if self.head is None:
-            self.head = item
-        else:
-            self.tail.next = item
-        self.tail = item
+ def print_all_nodes(self):
+    node = self.head
+    while node != None:
+     print(node.value)
+     node = node.next
 
-    def print_all_nodes(self):
-        node = self.head
-        while node != None:
-            print(node.value)
-            node = node.next
+ def find(self, val):
+    node = self.head
+    while node is not None:
+     if node.value == val:
+        return node
+     node = node.next
+    return None
 
-    def find(self, val):
-        node = self.head
-        while node is not None:
-            if node.value == val:
-                return node
-            node = node.next
-        return None
+ def find_all(self, val):
+    list_val = []
+    leng = 0
+    if(self.head == None):
+     return
+    node = self.head
+    while node is not None:
+     if (node.value == val):
+        list_val.append(leng)
+     leng += 1
+     node = node.next
+    return list_val # здесь будет ваш код
 
-    def find_all(self, val):
-        list_val = []
-        node = self.head
-        while node is not None:
-            if (node.value == val):
-                list_val.append(val)
-            node = node.next
-        return list_val # здесь будет ваш код
-
-    def delete(self, val, all=False):
-        leng = 0
-        if(self.head == None):
-                return
-        past = node = self.head   
-        if(all == False):  
-            while node is not None:
-                if (node.value == val):
-                    if(leng == 0):
-                        self.head = self.head.next
-                        return
-                    if (node.next == None):
-                        self.tail = node
-                    past.next = node.next
-                    break
-                past = node
-                node = node.next
-                leng += 1
-        else:
-            while node is not None:
-                if node.next != None:
-                    if past.value == node.next.value and past.value == val:
-                        node.next = node.next.next
-                else:
-                    past=node = past.next
-                node=node.next
-            pass # здесь будет ваш код                        
-                    
-
-    def clean(self):
-        self.__init__()
-        pass # здесь будет ваш код
-
-    def len(self):
-        self.leng = 0
-        if(self.head == None):
+ def delete(self, val, all = False):
+    leng = 0
+    if(self.head == None):
+     return
+    past = node = self.head   
+    if(all == False):  
+     while node is not None:
+        if (node.value == val):
+         if(leng == 0):
+            self.head = self.head.next
             return
-        node = self.head
-        while node.next is not None:
-            node = node.next
-            self.leng +=1
-        return self.leng+1 # здесь будет ваш код
+         if (node.next == None):
+            self.tail = node
+         past.next = node.next
+         break
+        past = node
+        node = node.next
+        leng += 1
+    else:
+     while node is not None:
+        if (node.next != None):
+         if past.value == node.next.value and past.value == val:
+            node.next = node.next.next
+        else:
+         past=node = past.next
+        node=node.next
+     pass # здесь будет ваш код
 
+ def clean(self):
+    self.__init__()
+    pass # здесь будет ваш код
 
-    def insert(self, afterNode, newNode):
-        if(self.head == None):
-            self.tail = self.head = Node(newNode)
-        node = self.head
-        if(afterNode == None and node == None):
-            self.head = node
-            self.tail = self.head
-        while node.next is not None:
-            if(node.value == afterNode):
-                node.next = Node(newNode)
-                if(node.next.next != None):
-                    self.tail.next = node.next
-                    break
-            node = node.next   
-        pass # здесь будет ваш код
+ def len(self):
+    self.leng = 0
+    if(self.head == None):
+     return
+    node = self.head
+    while node.next is not None:
+     node = node.next
+     self.leng +=1
+    return self.leng+1 # здесь будет ваш код 
+    
+ def insert(self, afterNode, newNode):
+    if(afterNode == None or self.head == None):
+        self.head = Node(newNode, None)
+    node = self.head
+    while node is not None:
+        if(node.value == afterNode):
+            node.next = Node(newNode, node.next)
+            if(node.next.next == None):
+                self.tail = node.next
+            break
+        node = node.next
+    pass # здесь будет ваш код
